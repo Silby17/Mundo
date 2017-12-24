3a450f5bfc1e303f26df5fe28d704b02d172fefb
Yosef Silberhaft <yossisilb@gmail.com>
2017-12-24 20:38:49 +0200

    Added Window manager

--- app/src/main/java/com/silbytech/mundo/IntroductionActivity.java
+++ app/src/main/java/com/silbytech/mundo/IntroductionActivity.java
@@ -10,6 +10,8 @@ import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -27,6 +29,9 @@ public class IntroductionActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.introduction_layout);
         pager = findViewById(R.id.pager);
         dotsLayout = findViewById(R.id.id_layout_dots);
