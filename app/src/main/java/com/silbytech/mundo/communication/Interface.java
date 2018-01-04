01e4146018031a3c21b0c5f5a11f3bfce6f78948
Yosef Silberhaft <yossisilb@gmail.com>
2018-01-04 12:28:51 +0200

    added listing upload route

--- app/src/main/java/com/silbytech/mundo/communication/Interface.java
+++ app/src/main/java/com/silbytech/mundo/communication/Interface.java
@@ -1,11 +1,23 @@
 package com.silbytech.mundo.communication;
 
+import com.silbytech.mundo.entities.ListingsArray;
 import com.silbytech.mundo.responses.LoginResponse;
+import com.silbytech.mundo.responses.MessageResponse;
+import com.silbytech.mundo.responses.Response;
 
+import java.util.Map;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
+import retrofit2.http.PartMap;
 
 /************************************
  * Created by Yosef Silberhaft
@@ -23,4 +35,33 @@ public interface Interface {
                                    @Field("surname") String lastName,
                                    @Field("email") String email,
                                    @Field("password") String password);
+
+    @Multipart
+    @POST("/listing/")
+    Call<ResponseBody> uploadPhoto(@Part("desc") RequestBody description,
+                                   @Part("myFile") MultipartBody.Part myFile);
+
+    @Multipart
+    @POST("/listing/")
+    Call<MessageResponse> upload(
+            @Part("desc") String description,
+            @PartMap Map<String, RequestBody> map
+    );
+
+    @GET("/all/listings/")
+    Call<ListingsArray> getAllListings();
+
+    @GET("/testing")
+    Call<Response> getTesting();
+
+    @Multipart
+    @POST("/upload")
+    Call<ResponseBody> upload(
+            @Part("description") RequestBody description,
+            @Part MultipartBody.Part file);
+
+    @Multipart
+    @POST("/upload")
+    Call<Response> uploadImage(@Part("description") RequestBody description,
+                               @Part MultipartBody.Part image);
 }
