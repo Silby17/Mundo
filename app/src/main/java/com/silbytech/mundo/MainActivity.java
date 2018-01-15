46d6ebc48d2d09fe569391be1f9bbb60572826b1
Yosef Silberhaft <yossisilb@gmail.com>
2018-01-15 21:20:09 +0200

    added in switch case for fragments

--- app/src/main/java/com/silbytech/mundo/MainActivity.java
+++ app/src/main/java/com/silbytech/mundo/MainActivity.java
@@ -1,13 +1,76 @@
 package com.silbytech.mundo;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_layout);
+        final Toolbar toolbar = findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        BottomNavigationViewHelper.removeShiftMode(bottomNavigationView);
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                switch (item.getItemId()){
+                    case R.id.action_all:
+                        toolbar.setTitle("All");
+                        Fragment frag = new SettingsFragment();
+                        loadFragment(frag);
+                        return true;
+                    case R.id.action_account:
+                        toolbar.setTitle("Account");
+                        Fragment accountFrag = new SingleListingFragment();
+                        loadFragment(accountFrag);
+                        return true;
+                    case R.id.action_inbox:
+                        toolbar.setTitle("Inbox");
+                        return true;
+                    case R.id.action_settings:
+                        toolbar.setTitle("Settings");
+                        return true;
+                }
+                return false;
+            }
+        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        Bundle args = new Bundle();
+        args.putString("param1", "Yossi is the man");
+        fragment.setArguments(args);
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
 }
