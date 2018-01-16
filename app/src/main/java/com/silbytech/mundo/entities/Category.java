9fb4f60ba9c8169be364c045c5817e707111537f
Yosef Silberhaft <yossisilb@gmail.com>
2018-01-16 13:40:35 +0200

    initial commit

---
+++ app/src/main/java/com/silbytech/mundo/entities/Category.java
@@ -0,0 +1,37 @@
+package com.silbytech.mundo.entities;
+
+import com.google.gson.annotations.SerializedName;
+import java.io.Serializable;
+
+/************************************
+ * Created by Yosef Silberhaft
+ ************************************/
+public class Category implements Serializable {
+
+    @SerializedName("_id")
+    private String id;
+
+    @SerializedName("name")
+    private String name;
+
+    public Category(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
