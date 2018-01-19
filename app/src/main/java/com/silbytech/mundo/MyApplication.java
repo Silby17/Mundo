02b508ad251c306e0fb9ef0f6572625f2ef167c4
Yosef Silberhaft <yossisilb@gmail.com>
2018-01-19 14:02:06 +0200

    initial commit

---
+++ app/src/main/java/com/silbytech/mundo/MyApplication.java
@@ -0,0 +1,61 @@
+package com.silbytech.mundo;
+
+import android.app.Application;
+
+import java.io.File;
+
+/************************************
+ * Created by Yosef Silberhaft
+ ************************************/
+public class MyApplication extends Application {
+    private static MyApplication instance;
+    public static final String PREFS = "prefs";
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+    }
+
+    public static MyApplication getInstance() {
+        return instance;
+    }
+
+    /*******************************************************************************
+     * This method will clear all of the Application data from the device
+     *******************************************************************************/
+    public void clearApplicationData() {
+        File cacheDirectory = getCacheDir();
+        File applicationDirectory = new File(cacheDirectory.getParent());
+        if (applicationDirectory.exists()) {
+            String[] fileNames = applicationDirectory.list();
+            for (String fileName : fileNames) {
+                if (!fileName.equals("lib")) {
+                    deleteFile(new File(applicationDirectory, fileName));
+                }
+            }
+        }
+    }
+
+
+    /*******************************************************************************
+     * This method will delete all file of the app from the device
+     * @param file - file to remove
+     * @return - true at completion
+     ********************************************************************************/
+    public static boolean deleteFile(File file) {
+        boolean deletedAll = true;
+        if (file != null) {
+            if (file.isDirectory()) {
+                String[] children = file.list();
+                for (int i = 0; i < children.length; i++) {
+                    deletedAll = deleteFile(new File(file, children[i])) && deletedAll;
+                }
+            } else {
+                deletedAll = file.delete();
+            }
+        }
+        return deletedAll;
+    }
+}
