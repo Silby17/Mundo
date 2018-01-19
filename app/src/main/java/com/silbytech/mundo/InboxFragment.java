dca5e123590456d4883182c311984b078eb97147
Yosef Silberhaft <yossisilb@gmail.com>
2018-01-19 12:13:42 +0200

    initial commit

---
+++ app/src/main/java/com/silbytech/mundo/InboxFragment.java
@@ -0,0 +1,86 @@
+package com.silbytech.mundo;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class InboxFragment extends Fragment {
+    private String TAG = "InboxFragment";
+    private static final String userId = "userId";
+    private static final String userToken = "token";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public InboxFragment() {
+        // Required empty public constructor
+    }
+
+    /*******************************************************************
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment InboxFragment.
+     *******************************************************************/
+    public static InboxFragment newInstance(String param1, String param2) {
+        InboxFragment fragment = new InboxFragment();
+        Bundle args = new Bundle();
+        args.putString(userId, param1);
+        args.putString(userToken, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(userId);
+            mParam2 = getArguments().getString(userToken);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_inbox, container, false);
+    }
+
+
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            Log.d(TAG, "Loaded Fragment");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+}
