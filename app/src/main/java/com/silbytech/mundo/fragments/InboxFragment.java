86b0cc013816e508826b390dd578f674273a34e0
Yosef Silberhaft <yossisilb@gmail.com>
2018-01-19 15:55:17 +0200

    formatted

--- app/src/main/java/com/silbytech/mundo/fragments/InboxFragment.java
+++ app/src/main/java/com/silbytech/mundo/fragments/InboxFragment.java
@@ -1,4 +1,4 @@
-package com.silbytech.mundo;
+package com.silbytech.mundo.fragments;
 
 import android.content.Context;
 import android.net.Uri;
@@ -8,6 +8,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.silbytech.mundo.R;
 
 public class InboxFragment extends Fragment {
     private String TAG = "InboxFragment";
