ad423847c67e323ee207241d0c96010859f68a80
Yosef Silberhaft <yossisilb@gmail.com>
2018-01-19 12:21:29 +0200

    Added getting string from Prefs
    added inbox, settings frag to switch, passed user info to all frags

--- app/src/main/java/com/silbytech/mundo/MainActivity.java
+++ app/src/main/java/com/silbytech/mundo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.silbytech.mundo;
 
-import android.support.annotation.NonNull;
+import com.silbytech.mundo.fragments.AllCategoriesFragment;
+import com.silbytech.mundo.fragments.InboxFragment;
+import com.silbytech.mundo.fragments.SettingsFragment;
+import android.content.SharedPreferences;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -12,9 +15,16 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
-import com.silbytech.mundo.fragments.AllCategoriesFragment;
 
+/************************************
+ * Created by Yosef Silberhaft
+ ************************************/
 public class MainActivity extends AppCompatActivity {
+    private FragmentManager fragmentManager;
+    private String userID;
+    public static final String PREFS = "prefs";
+    public String userToken;
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,32 +33,53 @@ public class MainActivity extends AppCompatActivity {
         final Toolbar toolbar = findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
         toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+        fragmentManager = getSupportFragmentManager();
+
+        //FragmentTransaction transaction = fragmentManager.beginTransaction();
+        preferences = getSharedPreferences(PREFS, 0);
+
+        userID = preferences.getString("userId", "");
+        userToken = preferences.getString("token", "");
+
+        final Bundle userArgs = new Bundle();
+        userArgs.putString("userId", userID);
+        userArgs.putString("token", userToken);
+
+
+        /*//Starts the All Categories Fragment at start
+        Fragment frag = new AllCategoriesFragment();
+        loadFragment(frag);*/
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
-
         BottomNavigationViewHelper.removeShiftMode(bottomNavigationView);
 
 
+        //Switch Case for bottom Navigation onClicks
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem item) {
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                FragmentTransaction transaction = fragmentManager.beginTransaction();
                 switch (item.getItemId()){
                     case R.id.action_all:
-                        toolbar.setTitle("All");
                         Fragment frag = new AllCategoriesFragment();
+                        frag.setArguments(userArgs);
                         loadFragment(frag);
                         return true;
                     case R.id.action_account:
                         toolbar.setTitle("Account");
                         Fragment accountFrag = new SingleListingFragment();
+                        accountFrag.setArguments(userArgs);
                         loadFragment(accountFrag);
                         return true;
                     case R.id.action_inbox:
                         toolbar.setTitle("Inbox");
+                        Fragment inboxFragment = new InboxFragment();
+                        inboxFragment.setArguments(userArgs);
+                        loadFragment(inboxFragment);
                         return true;
                     case R.id.action_settings:
+                        Fragment settingsFrag = new SettingsFragment();
+                        settingsFrag.setArguments(userArgs);
+                        loadFragment(settingsFrag);
                         toolbar.setTitle("Settings");
                         return true;
                 }
@@ -56,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -65,14 +97,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loadFragment(Fragment fragment) {
-        // load fragment
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        Bundle args = new Bundle();
-        args.putString("param1", "Yossi is the man");
-        fragment.setArguments(args);
         transaction.replace(R.id.frame_container, fragment);
         transaction.addToBackStack(null);
         transaction.commit();
     }
 
+    /**************************************************
+     * Closes the app on Back Button Pressed
+     *************************************************/
+    @Override
+    public void onBackPressed() {
+        this.finish();
+    }
 }
