bcab942583f180f18c569d886dfa3488d3fb164c
Yosef Silberhaft <yossisilb@gmail.com>
2018-01-19 15:04:38 +0200

    initial commit

---
+++ app/src/main/java/com/silbytech/mundo/MyAccountFragment.java
@@ -0,0 +1,118 @@
+package com.silbytech.mundo;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.silbytech.mundo.adapters.ViewPagerAdapter;
+import com.silbytech.mundo.fragments.SettingsFragment;
+
+public class MyAccountFragment extends Fragment {
+    private String TAG = "MyAccountFragment";
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public MyAccountFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MyAccountFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MyAccountFragment newInstance(String param1, String param2) {
+        MyAccountFragment fragment = new MyAccountFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_my_account, container, false);
+        viewPager = view.findViewById(R.id.vpAccount);
+        viewPager.bringToFront();
+        tabLayout = view.findViewById(R.id.accountTabs);
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            Log.d(TAG, "Loaded MyAccountFragment");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+    private void setupViewPager(ViewPager viewPager){
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this.getFragmentManager());
+
+        adapter.addFragment(SettingsFragment.newInstance(), "Settings");
+        adapter.addFragment(SettingsFragment.newInstance(), "Settings 2");
+        viewPager.setAdapter(adapter);
+
+        viewPager.setCurrentItem(0);
+    }
+}
