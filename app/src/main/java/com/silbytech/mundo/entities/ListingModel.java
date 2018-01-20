a0d3cea58eeb76375e6994e75a08bbea617249b5
Yosef Silberhaft <yossisilb@gmail.com>
2018-01-20 18:22:40 +0200

    added location and active in model

--- app/src/main/java/com/silbytech/mundo/entities/ListingModel.java
+++ app/src/main/java/com/silbytech/mundo/entities/ListingModel.java
@@ -36,9 +36,16 @@ public class ListingModel implements Serializable {
     @SerializedName("imageUrl")
     private ArrayList<String> imageUrls;
 
+    @SerializedName("location")
+    private String location;
+
+    @SerializedName("active")
+    private boolean active;
+
     public ListingModel(String id, String creatorID, String title, String category,
                         String description, double dailyPrice, double weeklyPrice,
-                        double monthlyPrice, ArrayList<String> imageUrls) {
+                        double monthlyPrice, ArrayList<String> imageUrls,
+                        String location, boolean active) {
         this.id = id;
         this.creatorID = creatorID;
         this.title = title;
@@ -48,6 +55,8 @@ public class ListingModel implements Serializable {
         this.weeklyPrice = weeklyPrice;
         this.monthlyPrice = monthlyPrice;
         this.imageUrls = imageUrls;
+        this.location = location;
+        this.active = active;
     }
 
     public String getId() {
@@ -120,5 +129,21 @@ public class ListingModel implements Serializable {
 
     public void setImageUrls(ArrayList<String> imageUrls) {
         this.imageUrls = imageUrls;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
     }
 }
