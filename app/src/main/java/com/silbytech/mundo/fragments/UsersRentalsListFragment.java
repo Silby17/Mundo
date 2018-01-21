85a7f056369397796534eb30a01c20d625819fe6
Yosef Silberhaft <yossisilb@gmail.com>
2018-01-21 13:03:17 +0200

    initial commit

---
+++ app/src/main/java/com/silbytech/mundo/fragments/UsersRentalsListFragment.java
@@ -0,0 +1,85 @@
+package com.silbytech.mundo.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.silbytech.mundo.R;
+
+public class UsersRentalsListFragment extends Fragment {
+    private static final String USER_ID = "userId";
+    private static final String USER_TOKEN = "userToken";
+    private String userId;
+    private String userToken;
+
+    private OnFragmentInteractionListener mListener;
+
+    public UsersRentalsListFragment() {}
+
+    public static UsersRentalsListFragment newInstance(String userId, String userToken) {
+        UsersRentalsListFragment fragment = new UsersRentalsListFragment();
+        Bundle args = new Bundle();
+        args.putString(USER_ID, userId);
+        args.putString(USER_TOKEN, userToken);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            userId = getArguments().getString(USER_ID);
+            userToken = getArguments().getString(USER_TOKEN);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_users_rentals_list, container, false);
+    }
+
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
