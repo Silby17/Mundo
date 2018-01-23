d61ee48443c6863bd2c02095083052382bacb88a
Yosef Silberhaft <yossisilb@gmail.com>
2018-01-23 17:49:14 +0200

    Changed fragment in setPager

--- app/src/main/java/com/silbytech/mundo/fragments/MyAccountFragment.java
+++ app/src/main/java/com/silbytech/mundo/fragments/MyAccountFragment.java
@@ -1,11 +1,8 @@
-package com.silbytech.mundo;
+package com.silbytech.mundo.fragments;
 
 import android.content.Context;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.RequiresApi;
-import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
@@ -14,8 +11,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.silbytech.mundo.R;
+import com.silbytech.mundo.UserListingFragment;
 import com.silbytech.mundo.adapters.ViewPagerAdapter;
-import com.silbytech.mundo.fragments.SettingsFragment;
 
 public class MyAccountFragment extends Fragment {
     private String TAG = "MyAccountFragment";
@@ -93,9 +91,8 @@ public class MyAccountFragment extends Fragment {
 
         String tok = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YTMyODFjNDU3ZjhjZTI1MWM3NGE4YzgiLCJhY2Nlc3MiOiJhdXRoIiwiaWF0IjoxNTE2NDY1NjI4fQ.kHitKHjAwSZCPVI2HRlkHPD8Djs-PdPCc59PcqMQ8_c";
         adapter.addFragment(UserListingFragment.newInstance(userId, tok), "My Listings");
-        adapter.addFragment(SettingsFragment.newInstance(), "My Rentals");
+        adapter.addFragment(UsersRentalsListFragment.newInstance(userId, tok), "My Rentals");
         viewPager.setAdapter(adapter);
-
         viewPager.setCurrentItem(0);
     }
 }
