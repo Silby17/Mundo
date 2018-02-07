66b4c188fcfa8dc8315168014a3944ec51f9c273
Yosef Silberhaft <yossisilb@gmail.com>
2018-02-07 10:22:16 +0200

    initial commit

---
+++ app/src/main/java/com/silbytech/mundo/AddListingScreenThreeActivity.java
@@ -0,0 +1,15 @@
+package com.silbytech.mundo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/************************************
+ * Created by Yosef Silberhaft
+ ************************************/
+public class AddListingScreenThreeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
