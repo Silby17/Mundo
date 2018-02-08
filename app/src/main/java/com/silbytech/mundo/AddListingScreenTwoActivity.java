b76c285cd82df766ca94930125d331d78a546137
Yosef Silberhaft <yossisilb@gmail.com>
2018-02-08 14:34:15 +0200

    Removed google map and added image upload

--- app/src/main/java/com/silbytech/mundo/AddListingScreenTwoActivity.java
+++ app/src/main/java/com/silbytech/mundo/AddListingScreenTwoActivity.java
@@ -1,45 +1,52 @@
 package com.silbytech.mundo;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.location.Address;
-import android.location.Geocoder;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.provider.MediaStore;
 import android.util.DisplayMetrics;
 import android.util.Log;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.ui.PlaceAutocomplete;
-import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
-import com.google.android.gms.location.places.ui.PlaceSelectionListener;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
 import com.shuhart.stepview.StepView;
 import com.silbytech.mundo.entities.ListingModel;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
 
 /************************************
  * Created by Yosef Silberhaft
  ************************************/
-public class AddListingScreenTwoActivity extends FragmentActivity implements OnMapReadyCallback{
-    ListingModel newListing;
-    private GoogleMap mMap;
+public class AddListingScreenTwoActivity extends Activity {
     public String TAG = "AddListingScreenTwoActivity";
-    public int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
+    ListingModel newListing;
+    private StepView stepsView;
+    Integer REQUEST_CAMERA = 1, REQUEST_GALLERY = 2;
+    List<Uri> imageList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_listing_screen_two);
+        Button btnNext = findViewById(R.id.btnNext);
+        EditText listingTitle = findViewById(R.id.edtTitle);
+        EditText listingDesc = findViewById(R.id.edtDescription);
+        ImageView listingImg = findViewById(R.id.imgUpload);
+        Button btnChooseImg = findViewById(R.id.btnSelectImage);
 
-        final StepView stepsView = findViewById(R.id.stepsViewTwo);
+        stepsView = findViewById(R.id.stepsViewTwo);
+        initStepView();
         newListing = (ListingModel)getIntent().getSerializableExtra("NewListing");
 
         //Display the screen in a pop-up overlay of the screen
@@ -51,87 +58,106 @@ public class AddListingScreenTwoActivity extends FragmentActivity implements OnM
         //Sets the minimized screen
         getWindow().setLayout((int)(width * .9), ((int)(height * .8)));
 
-
-        List<String> stepsArray = Arrays.asList(getResources().getStringArray(R.array.steps_array));
-        stepsView.getState()
-                .selectedTextColor(getApplicationContext().getResources().getColor(R.color.black))
-                .doneTextColor(getApplicationContext().getResources().getColor(R.color.white))
-                .animationType(StepView.ANIMATION_LINE)
-                .nextTextColor(getApplicationContext().getResources().getColor(R.color.black))
-                .selectedCircleColor(getApplicationContext().getResources().getColor( R.color.colorAccent))
-                .selectedStepNumberColor(getApplicationContext().getResources().getColor( R.color.colorPrimary))
-                .steps(stepsArray)
-                .animationDuration(getResources().getInteger(android.R.integer.config_shortAnimTime))
-                // other state methods are equal to the corresponding xml attributes
-                .commit();
-        stepsView.go(1, true);
-
-        //Init the Support Fragment For the Map
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
-        //Init the PlaceAutoCompleteFragment for the autocomplete GoogleMaps search
-        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
-                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
-
-        //The Google-Maps auto complete handler
-        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+        btnChooseImg.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onPlaceSelected(Place place) {
-                Log.i(TAG, "Place: " + place.getName());
-                LatLng pickedPlace = place.getLatLng();
-                changeLocation(pickedPlace);
+            public void onClick(View view) {
+                selectImage();
             }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onError(Status status) {
-                Log.i(TAG, "An error occurred: " + status);
+            public void onClick(View view) {
+                Intent i = new Intent(AddListingScreenTwoActivity.this, AddListingScreenThreeActivity.class);
+                i.putExtra("NewListing", newListing);
+                startActivity(i);
             }
         });
     }
 
 
-    public void changeLocation(LatLng currentPosition){
-        mMap.addMarker(new MarkerOptions().position(currentPosition)
-                .title("Marker Picked"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentPosition));
-        mMap.setMinZoomPreference(13.0f);
+    private void selectImage(){
+        //Sets the options for choosing an image
+        final CharSequence[] items = {"Gallery", "Camera", "Cancel"};
+        //Build a dialog to choose image
+        AlertDialog.Builder builder = new AlertDialog.Builder(AddListingScreenTwoActivity.this);
+        builder.setTitle("Choose an image");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //User clicks Camera
+                if(items[i].equals("Camera")){
+                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, REQUEST_CAMERA);
+                }
+                //User clicks Gallery
+                else if(items[i].equals("Gallery")){
+                    Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                    intent.setType("image/jpeg");
+                    try {
+                        startActivityForResult(intent, REQUEST_GALLERY);
+                    } catch (ActivityNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    //User clicks cancel
+                } else if(items[i].equals("Cancel")){
+                    dialogInterface.dismiss();
+                }
+            }
+        });
+        //Shows the image chooser dialog
+        builder.show();
     }
-
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        Geocoder gc = new Geocoder(getApplicationContext());
-        List<Address> addresses = new ArrayList<>();
-        try {
-            addresses = gc.getFromLocationName("Tel Aviv Israel", 2);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println(addresses.size());
-        // Add a marker in Sydney, Australia,
-        // and move the map's camera to the same location.
-        mMap = googleMap;
-        LatLng listingLocation = new LatLng(addresses.get(0).getLatitude(), addresses.get(0).getLongitude());
-        mMap.setMinZoomPreference(13.5f);
-        mMap.addMarker(new MarkerOptions().position(listingLocation)
-                .title("Listing"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(listingLocation));
-    }
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                Place place = PlaceAutocomplete.getPlace(this, data);
-                Log.i(TAG, "Place: " + place.getName());
-            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
-                Status status = PlaceAutocomplete.getStatus(this, data);
-                Log.i(TAG, status.getStatusMessage());
-            } else if (resultCode == RESULT_CANCELED) {
-                // The user canceled the operation.
+        if (requestCode == REQUEST_GALLERY && resultCode == RESULT_OK) {
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
+                ImageView imgItem = findViewById(R.id.imgItem);
+                imgItem.setImageBitmap(bitmap);
+                this.imageList.add(data.getData());
+                Log.d("Size of Image List is", Integer.toString(imageList.size()));
+                InputStream is = getContentResolver().openInputStream(data.getData());
+                //uploadImage(getBytes(is));
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
+        else if(requestCode == REQUEST_CAMERA && resultCode == RESULT_OK){
+        }
+    }
+
+    public byte[] getBytes(InputStream is) throws IOException {
+        ByteArrayOutputStream byteBuff = new ByteArrayOutputStream();
+        int buffSize = 1024;
+        byte[] buff = new byte[buffSize];
+
+        int len = 0;
+        while ((len = is.read(buff)) != -1) {
+            byteBuff.write(buff, 0, len);
+        }
+        return byteBuff.toByteArray();
+    }
+
+
+    /*********************************************************************
+     * This function will initialize the step view
+     *********************************************************************/
+    public void initStepView(){
+        List<String> stepsArray = Arrays.asList(getResources().getStringArray(R.array.steps_array));
+        stepsView.getState()
+                .selectedTextColor(getApplicationContext().getResources().getColor(R.color.white))
+                .doneTextColor(getApplicationContext().getResources().getColor(R.color.white))
+                .animationType(StepView.ANIMATION_LINE)
+                .nextTextColor(getApplicationContext().getResources().getColor(R.color.black))
+                .selectedCircleColor(getApplicationContext().getResources().getColor( R.color.colorAccent))
+                .doneStepLineColor(getApplicationContext().getResources().getColor(R.color.white))
+                .selectedStepNumberColor(getApplicationContext().getResources().getColor( R.color.colorPrimary))
+                .steps(stepsArray)
+                .animationDuration(getResources().getInteger(android.R.integer.config_shortAnimTime))
+                .commit();
+
+        stepsView.go(1, false);
     }
 }
